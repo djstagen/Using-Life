e20960497969f6c81ca2fef7652b172c84ac46b6
djstagen <dstagen@colum.edu>
2020-10-08 16:29:34 -0400

    Done

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    public Transform WeaponStart;
+    public GameObject WeaponPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            Debug.Log("Insert Weapon swing");
+        }
+    }
+
+    void Shoot()
+    {
+        // shooting logic
+        Instantiate(WeaponPrefab, WeaponStart.position, WeaponStart.rotation);
+    }
+}
