211930f3ce7e1cec5b40a3bd86a4dd2d64d3cf61
EfisEvisule <58088743+djstagen@users.noreply.github.com>
2020-10-15 10:04:35 -0500

    Added attack, dead slime prefab, honestly this should be multiple commitsI got excited

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,28 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health = 10;
+    public int health;
+    public float speed;
+    public GameObject deadEnemyLand;
 
-    public GameObject deathEffect;
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            Instantiate(deadEnemyLand, transform.position,Quaternion.identity);
+        }
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+
 
     public void TakeDamage(int damage)
     {
         health -= damage;
-
-        if (health <= 0)
-        {
-            Die();
-        }
-    }
-
-    void Die()
-    {
-        Instantiate(deathEffect, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        Debug.Log("damage was dealt to enemy");
     }
 }
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeAttackCD;
+    public float startAttacktime;
+
+    public Transform attackPosition;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+    //code referenced from blackthornprod
+
+    private void Update()
+    {
+        if (timeAttackCD <= 0)
+        {
+            if(Input.GetKey(KeyCode.J))
+            {
+                
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage (damage);
+                }
+            }
+            timeAttackCD = startAttacktime;
+        }
+        else
+        {
+            timeAttackCD -= Time.deltaTime;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+}
