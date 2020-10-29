91502ace7ad3ea17b1a969477ce09e46629278c3
EfisEvisule <58088743+djstagen@users.noreply.github.com>
2020-10-29 15:26:58 -0500

    Updated player heal, still in progress

--- Assets/Scripts/PlayerHeal.cs
+++ Assets/Scripts/PlayerHeal.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerHeal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timeHealCD;
+    public float startHealTime;
 
-    // Update is called once per frame
-    void Update()
+    public Transform healPosition;
+    public LayerMask whatIsDeadBody;
+    public float healRange;
+    public int heal;
+
+    public GameObject healParticles;
+
+    private void Update()
     {
-        
+        if (timeHealCD <= 0)
+        {
+            if (Input.GetKey(KeyCode.H))
+            {
+                Instantiate(healParticles, healPosition.position, Quaternion.identity);
+                Collider2D[] enemiesToHeal = Physics2D.OverlapCircleAll(healPosition.position, healRange, whatIsDeadBody);
+                for (int i = 0; i < enemiesToHeal.Length; i++)
+                {
+
+                }
+            }
+        }
     }
 }
