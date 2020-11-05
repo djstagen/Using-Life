29e225c033eb2a48a97a0ad93b9effc8fe33ba8b
EfisEvisule <58088743+djstagen@users.noreply.github.com>
2020-11-05 10:38:52 -0600

    Added Pressure Plate for player

---
+++ Assets/Scripts/DoorSetActive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSetActive : MonoBehaviour, IDoor
+{
+    [SerializeField]
+    GameObject door;
+
+    private bool isOpen = false;
+
+    public void OpenDoor()
+    {
+
+    }
+
+    public void CloseDoor()
+    {
+
+    }
+
+    public void ToggleDoor()
+    {
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+}
