57ac8b81361890ccee8fd7571bf96ec0e2488ca3
Dj <58088743+djstagen@users.noreply.github.com>
2020-12-10 13:31:23 -0600

    Tooltips were added

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Text : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        uiObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/ToolTip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ToolTip : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    public void OnTriggerEnter2D( )
+    {
+        uiObject.SetActive(true);
+    }
+
+    public void OnTriggerExit2D()
+    {
+        uiObject.SetActive(false);
+    }
+}
